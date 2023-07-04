13de184407466a1ebac3fbecdec4b675c464eaf0
Aaronfan56 <138613578+Aaronfan56@users.noreply.github.com>
2023-07-04 19:17:20 -0400

    Update Core.client.lua

--- StarterPlayer/StarterPlayerScripts/Core.client.lua
+++ StarterPlayer/StarterPlayerScripts/Core.client.lua
@@ -1,2 +1,2 @@
 local StarterGui = game:GetService("StarterGui")
-StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false) -- Set it to What You Want.
